1a3c34074ee03b6f68882e8b20b567387d3df1d9
Mathias Minacapilli <Mathias@LAN-of-Coffee.local>
2019-09-05 11:56:54 -0300

    Added the functionality to show an individual Post selecting it and when this happens the app will send a request in order to get the data of all the post and show it in the DOM

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
 
-        );
+    state = {
+        postLoaded: false,
+    }
+
+    componentDidUpdate() {
+        if(this.props.postId) {
+            // This if is in order to not send requests all the time
+            if(!this.state.postLoaded || (this.state.postLoaded && this.state.postLoaded.id !== this.props.postId))
+            axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.postId)
+                .then(response => {
+                    //console.log(response);
+                    this.setState({postLoaded: response.data});
+                });
+        }
+    }
+
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        // We have a post selected but not the information yet
+        if(this.props.postId) {
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+        // We now have the information
+        if(this.state.postLoaded) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.postLoaded.title}</h1>
+                    <p>{this.state.postLoaded.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,39 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts: [],
+        selectedPostId: false,
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({posts: response.data.slice(0, 4)})
+                console.log(response);
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post 
+                key={post.id} 
+                title={post.title}
+                clicked={() => this.postSelectedHandler(post.id)} />
+        })
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost postId={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
